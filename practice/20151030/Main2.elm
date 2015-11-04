1ac63ee294c7cebb244ab02726cd9f02a8c2a308
Tom Kidd <tomjkidd@gmail.com>
2015-11-04 09:03:20 -0500

    Adding usage of Font Awesome for expand and collapse proof of concept

---
+++ practice/20151030/Main2.elm
@@ -0,0 +1,64 @@
+-- The goal here is just to get Font Awesome working in isolation
+import StartApp
+import Task
+import Effects
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+import DynamicStyle
+
+type alias Model =
+    { text : String
+    , expanded: Bool
+    }
+
+init = (Model "This is a basis for expand/collapse functionality..." False, Effects.none)
+
+type Action = NoOp | ToggleExpand
+
+update action model =
+    case action of
+        NoOp -> ( model , Effects.none )
+        ToggleExpand -> ( { model | expanded <- not model.expanded }, Effects.none )
+
+{- Help from the internet
+http://stackoverflow.com/questions/29030951/how-do-you-add-a-hover-effect-on-elm-without-using-signals
+
+http://stackoverflow.com/questions/18836989/why-does-background-colornone-not-override-a-specified-background-color
+
+-}
+view address model =
+    let expanderAttrs =
+        [ classList
+            [ ("fa", True)
+            , ("fa-arrow-right", not model.expanded)
+            , ("fa-arrow-down", model.expanded)
+            ]
+        , onClick address ToggleExpand
+        , style [ ("cursor", "pointer") ]
+        ]
+    in
+        div (DynamicStyle.hover [ ("background-color", "transparent", "lightgrey") ])
+            [ i expanderAttrs []
+            , span [ style
+                [ ("padding-left", "5px")
+                , ("cursor", "default")
+                ]
+              ] [text model.text]
+            ]
+
+app =
+  StartApp.start
+    { init = init
+    , update = update
+    , view = view
+    , inputs = []
+    }
+
+main =
+  app.html
+
+port tasks : Signal (Task.Task Effects.Never ())
+port tasks =
+  app.tasks
