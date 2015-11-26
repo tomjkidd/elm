58101c7fcaf8d040bfe19f7518556cb3f2b908ce
Tom Kidd <tomjkidd@gmail.com>
2015-11-26 15:15:57 -0500

    Adding proof of concept code for location indexed tree

---
+++ practice/tree-work/MultiwayTreeTransform.elm
@@ -0,0 +1,41 @@
+module MultiwayTreeTransform where
+
+import Utils exposing (..)
+import MultiwayTree exposing (..)
+import MultiwayTreeData exposing (..)
+import MultiwayTreeZipper exposing (goToChildWithDefault, goToRoot, Zipper)
+
+-- TODO: This function has a lot of potential for learning with respect to being more general!
+{-| Create a structure that allows a function to be applied with a list accumulator
+-}
+mapAccum fn (Tree datum children) accum =
+    let
+        indexedChildren = indexedZip children
+        mappedChildren = List.map
+            (\(child, index) ->
+                mapAccum fn child (index :: accum))
+            indexedChildren
+    in
+        fn datum mappedChildren accum
+
+{-| A tree in this style will have the leaf-to-root path into the tree as the
+value in accum. This type of structure can be used to allow Actions from the UI
+to traverse to the node in order to update it through a Zipper.
+-}
+transformModel model = mapAccum
+    (\datum children accum ->
+        Tree (datum, accum) children
+    )
+    model
+    []
+
+goToChild : Int -> Zipper (String, List Int) -> Zipper (String, List Int)
+goToChild = goToChildWithDefault (Tree ("ERROR!",[]) [])
+
+goUp = MultiwayTreeZipper.goUp
+
+goToRoot = MultiwayTreeZipper.goToRoot
+
+transformedTree = transformModel initTree
+
+test = (transformedTree, []) |> goToChild 0
